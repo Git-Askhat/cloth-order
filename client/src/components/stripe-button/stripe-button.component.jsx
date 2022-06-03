3a7ca5617035b60fe0cfdede67615fb82a174620
Git-Askhat <askat.shailoobekovich@mail.ru>
2022-06-03 14:18:31 +0600

    Final commit

--- client/src/components/stripe-button/stripe-button.component.jsx
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -3,28 +3,28 @@ import StripeCheckout from 'react-stripe-checkout';
 import axios from 'axios';
 
 const StripeCheckoutButton = ({ price }) => {
-  const priceForStripe = price * 100;
-  const publishableKey = 'pk_test_b7a3hFL5nC3qlBCZ6bQACpez00gyMMP52H';
+  // const priceForStripe = price * 100;
+  // const publishableKey = 'pk_test_b7a3hFL5nC3qlBCZ6bQACpez00gyMMP52H';
 
-  const onToken = token => {
-    axios({
-      url: '/payment',
-      method: 'post',
-      data: {
-        amount: priceForStripe,
-        token: token
-      }
-    })
-      .then(response => {
-        alert('Succesful Payment!');
-      })
-      .catch(error => {
-        console.log('Payment Error: ', error);
-        alert(
-          'There was an issue with your payment! Please make sure to use the provided credit card data'
-        );
-      });
-  };
+  // const onToken = token => {
+  //   axios({
+  //     url: '/payment',
+  //     method: 'post',
+  //     data: {
+  //       amount: priceForStripe,
+  //       token: token
+  //     }
+  //   })
+  //     .then(response => {
+  //       alert('Succesful Payment!');
+  //     })
+  //     .catch(error => {
+  //       console.log('Payment Error: ', error);
+  //       alert(
+  //         'There was an issue with your payment! Please make sure to use the provided credit card data'
+  //       );
+  //     });
+  // };
 
   return (<></>)
     // <StripeCheckout
